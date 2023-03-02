b405606edc07470b95d3bed82d81f7f7bc8de4d4
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-03-02 16:20:49 +0600

    added important section

--- js/news-portal.js
+++ js/news-portal.js
@@ -8,7 +8,7 @@ const fetchCategories = () => {
   const showCategories = data =>{
     const categoriesContainer = document.getElementById("categories-container");
     data.news_category.forEach(singleCategory => {
-        console.log(singleCategory);
+        // console.log(singleCategory);
         // rules 1
         // categoriesContainer.innerHTML += `<a class="nav-link" href="#">${singleCategory.category_name}</a>`
         // rules 2
@@ -23,10 +23,51 @@ const fetchCategories = () => {
     const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`;
     fetch(url)
     .then(res => res.json())
-    .then (data =>console.log(data, category_name))
-  }
+    .then (data =>showAllData(data.data, category_name))
+  };
   const showAllData = (data, category_name ) => {
 // console.log(data, category_name);
-document.getElementById('news-count').innerText = data.length
-document.getElementById('category-name').innerText = category_name
-  }
+document.getElementById('news-count').innerText = data.length;
+document.getElementById('category-name').innerText = category_name;
+
+const newsContainer = document.getElementById("all-news");
+newsContainer.innerHTML = "";
+data.forEach((singleNews) => {
+    // console.log(singleNews);
+    const { image_url,title,details,author,total_view} = singleNews;
+    const card = document.createElement("div");
+    card.classList.add("card", "mb-3");
+    card.innerHTML = `<div class="row g-0">
+            <div class="col-md-4">
+              // <img src=${image_url} class="img-fluid rounded-start" alt="..."/>
+            </div>
+            <div class="col-md-8 d-flex flex-colum">
+              <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <p class="card-text">${details.slice(0, 200)}...</p>
+                <div class="card-footer border-0 bg-body d-flex justify-content-between">
+                <div>
+                <img src=${author.img} class="img-fluid rounded-start" alt="..." height="40" width="40"/>
+                <div>
+                <p class="m-0 p-0">${author.name}</p>
+                <p class="m-0 p-0">${author.publish_date}</p>
+                </div>
+              </div>
+                <div class="d-flex align-items-center">
+                <i class="fas fa-eye"></i>
+                <p class="m-0 p-0">${total_view}</p>
+                </div>
+                <div>
+                <i class="fas fa-star"></i>
+                </div>
+                <div>
+                <i class="fas fa-arrow"></i>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+        
+    newsContainer.appendChild(card);
+})
+  };
